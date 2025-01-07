5e86db0377408bf1fcf2b849a9a90ef913f26e51
Jurgen <jurgen.baas@outlook.com>
2025-01-07 22:52:03 +0100

    feat: Initial release. Territory of the demolisher is invisible.

---
+++ data.lua
@@ -0,0 +1,12 @@
+local default_enemy_color = data.raw["utility-constants"]["default"]["chart"]["default_enemy_color"]
+local unit_types = {"segment", "segmented-unit", "spider-unit", "unit"}
+for _, unit_type in pairs(unit_types) do
+    for _, prototype in pairs(data.raw[unit_type]) do
+        if not prototype.enemy_map_color then
+            prototype.enemy_map_color = default_enemy_color
+        end
+    end
+end
+
+data.raw["utility-constants"]["default"]["chart"]["default_enemy_territory_color"] = {0, 0, 0, 0}
+data.raw["utility-constants"]["default"]["chart"]["default_enemy_color"] = {0, 0, 0, 0}
